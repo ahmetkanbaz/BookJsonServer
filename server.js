41f1a91bc969bb446a3653f681f59e43ea226b02
Ahmet KANBAZ <ahmetkanbaz@gmail.com>
2023-12-15 22:32:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('books.json');
+const router = jsonServer.router("books.json");
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
-server.use(router)
+server.use(router);
 
-server.listen(3000)
+server.listen(port);
